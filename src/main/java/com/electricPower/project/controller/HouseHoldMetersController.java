49a9623533136e1ba6484f8a7656e5c338ca10c4
ChaFan <2771508305@qq.comm>
2020-10-04 23:19:44 +0800

    基本接口

--- src/main/java/com/electricPower/project/controller/HouseHoldMetersController.java
+++ src/main/java/com/electricPower/project/controller/HouseHoldMetersController.java
@@ -1,11 +1,16 @@
 package com.electricPower.project.controller;
 
 
+import com.electricPower.common.result.CommonResult;
+import com.electricPower.project.entity.HouseHoldMeters;
+import com.electricPower.project.entity.Terminal;
+import com.electricPower.project.service.IHouseHoldMetersService;
+import com.electricPower.project.service.ITerminalService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -20,5 +25,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/house-hold-meters")
 @Slf4j
 public class HouseHoldMetersController {
+
+    @Autowired
+    private IHouseHoldMetersService houseHoldMetersService;
+
+    @ApiOperation("查询所有的管理单元")
+    @GetMapping("/getList")
+    public CommonResult getList() {
+        return CommonResult.success(houseHoldMetersService.list());
+    }
+
+    @ApiOperation("根据Id查询所有的管理单元")
+    @GetMapping("/getHouseholdSn")
+    public CommonResult get(String householdSn) {
+        return CommonResult.success(houseHoldMetersService.getById(householdSn));
+    }
+
+    @ApiOperation("添加查管理单元")
+    @PostMapping("/add")
+    public CommonResult add(@RequestBody HouseHoldMeters houseHoldMeters) {
+        return CommonResult.success(houseHoldMetersService.save(houseHoldMeters));
+    }
+
+    @ApiOperation("更新查管理单元")
+    @PutMapping("/update")
+    public CommonResult update(@RequestBody HouseHoldMeters houseHoldMeters) {
+
+        return CommonResult.success(houseHoldMetersService.updateById(houseHoldMeters));
+    }
+
+    @ApiOperation("通过id删除查管理单元")
+    @DeleteMapping("/delete")
+    public CommonResult delete(String householdSn) {
+
+        return CommonResult.success(houseHoldMetersService.removeById(householdSn));
+    }
+
+
+
 
 }
--- src/main/java/com/electricPower/project/controller/TerminalController.java
+++ src/main/java/com/electricPower/project/controller/TerminalController.java
@@ -1,11 +1,15 @@
 package com.electricPower.project.controller;
 
 
+import com.electricPower.common.result.CommonResult;
+import com.electricPower.project.entity.PowerBureau;
+import com.electricPower.project.entity.Terminal;
+import com.electricPower.project.service.ITerminalService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -20,5 +24,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("//terminal")
 @Slf4j
 public class TerminalController {
+
+    @Autowired
+    private ITerminalService terminalService;
+
+    @ApiOperation("查询所有的巡检机器人")
+    @GetMapping("/getALLType")
+    public CommonResult getList() {
+        return CommonResult.success(terminalService.list());
+    }
+
+    @ApiOperation("根据Id查询巡检机器人")
+    @GetMapping("/getPowerBureauId")
+    public CommonResult get(String terminalNum) {
+        return CommonResult.success(terminalService.getById(terminalNum));
+    }
+
+    @ApiOperation("添加巡检机器人")
+    @PostMapping("/add")
+    public CommonResult add(@RequestBody Terminal terminal) {
+        return CommonResult.success(terminalService.save(terminal));
+    }
+
+    @ApiOperation("更新巡检机器人")
+    @PutMapping("/update")
+    public CommonResult update(  @RequestBody Terminal terminal) {
+
+        return CommonResult.success(terminalService.updateById(terminal));
+    }
+
+    @ApiOperation("通过id删除巡检机器人")
+    @DeleteMapping("/delete")
+    public CommonResult delete(String terminalNum) {
+
+        return CommonResult.success(terminalService.removeById(terminalNum));
+    }
+
+
+
+
+
+
+
+
+
+
 
 }

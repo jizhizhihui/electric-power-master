5e0aad79799829c49f68f553720a963ed4f716f3
jizhizhihui <1340355186@qq.com>
2020-10-30 11:56:53 +0800

    更新Socket

--- src/main/java/com/electricPower/core/socket/constants/SocketConstant.java
+++ src/main/java/com/electricPower/core/socket/constants/SocketConstant.java
@@ -6,7 +6,7 @@ public class SocketConstant {
 	/**
 	 * 心跳频率为 4 分钟
 	 */
-	public static final int HEART_RATE = 10*1000;
+	public static final int HEART_RATE = 4*60*1000;
 
 	/**
 	 * 最多开2000个socket线程，超过的直接拒绝
